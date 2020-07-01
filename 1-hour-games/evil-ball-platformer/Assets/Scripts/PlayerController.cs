21ad41c06bbe9892dc64ae578a9b1e03d0af4225
lindalen <haibonmusic@hotmail.com>
2020-07-01 14:36:13 +0200

    Done! 54 minutes spent. Important missing features: sound effects and animations. General comments: Took less time than expected to create the PlayerController and various colliders. I chose to prioritize functionality over robustness. Things are named in strange ways, The art looks a bit funky. I probably use more colliders than I need. Stuff that can be made into prefabs are not prefabbed. All in all an interesting project.

---
+++ 1-hour-games/evil-ball-platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        float moveAmplifier = 10f;
+        float moveForce = input * moveAmplifier;
+
+        Vector2 moveVector = new Vector2(moveForce, rb.velocity.y);
+        rb.velocity = moveVector;
+    }
+
+    private void Jump()
+    {
+        float jumpForce = 7f;
+
+        Vector2 jumpVector = new Vector2(rb.velocity.x, jumpForce);
+        rb.velocity = jumpVector;
+    }
+}
