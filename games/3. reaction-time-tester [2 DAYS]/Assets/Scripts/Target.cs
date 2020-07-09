0f0f0a0b1d31dfb0891a7a3ea1e0af261f0488db
lindalen <haibonmusic@hotmail.com>
2020-07-09 20:58:50 +0200

    Timer start 20:30 09/07/2020. Smaller project that should be completable within the time limit. The game tests your reaction time and mouse accuracy. Art will be minimal. If theres is excess time, animations and sound effects will be added. This commit adds a script for target behaviour and some defauly folders.

---
+++ games/3. reaction-time-tester [2 DAYS]/Assets/Scripts/Target.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] private float secsUntilDestruction;
+
+    private float lifeTime;
+
+    void Start()
+    {
+        secsUntilDestruction = 0.5f;
+        lifeTime = Time.time + secsUntilDestruction;
+    }
+
+    void Update()
+    {
+        LifeCycle();
+    }
+
+    void LifeCycle()
+    {
+        if (lifeTime > Time.time) return;
+        DestroySelf();
+
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    //TODO: Add function that destroys self, but first registers successful click
+}
