04abc426b1bde46c6d445f1291f8b9c25ad6e513
lindalen <haibonmusic@hotmail.com>
2020-07-15 18:06:32 +0200

    Adds prefab for paths. Adds prefab variants for bottompath and toppath. Adds script for path behaviour. Adds script for waypoint behaviour. Paths ready for use in enemy movement.

---
+++ games/4. Tower Defense Game [1 WEEK]/Assets/Scripts/UI/Path.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> waypoints;
+    private List<Vector3> waypointPositions;
+
+    private void Start()
+    {
+        waypointPositions = new List<Vector3>();
+        Debug.Log(waypoints.Count);
+        InitWaypointList();
+    }
+
+    private void InitWaypointList()
+    {
+        foreach (GameObject go in waypoints)
+        {
+            // perhaps over the top, but its easier to work with
+            waypointPositions.Add(go.transform.position);
+        }
+    }
+}
---
+++ games/4. Tower Defense Game [1 WEEK]/Assets/Scripts/UI/Waypoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    private Vector3 position;
+
+    private void Start()
+    {
+        position = transform.position;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return position;
+    }
+    //TODO: create method for getting this waypoints position + random small margin
+}
