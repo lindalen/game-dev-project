a7ddf55696dd3f27e3b4342ed7908769cbc002cf
lindalen <haibonmusic@hotmail.com>
2020-08-15 21:33:38 +0200

    DAY 1: Mostly planning and setup.

---
+++ games/no-name-yet [2 WEEKS]/Assets/Scripts/Interfaces/IGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGenerator
+{
+    float currencyPerSecond { get; set; }
+    float upgradeMultiplier { get; set; }
+
+    void GenerateCurrency();
+
+}
