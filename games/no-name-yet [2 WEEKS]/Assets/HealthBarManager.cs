cad2561a32f07c7dd5ae4bd8d69192338b691e21
lindalen <haibonmusic@hotmail.com>
2020-08-16 15:16:14 +0200

    DAY 2: Adds various ScirptableObjects for variables. Adds health bar that can work for both player and enemy health. Adds panel for displaying stats

---
+++ games/no-name-yet [2 WEEKS]/Assets/HealthBarManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarManager : MonoBehaviour
+{
+    [SerializeField] FloatVariable maxHP;
+    [SerializeField] FloatVariable currentHP;
+    [SerializeField] Text healthText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(currentHP.RuntimeValue / maxHP.RuntimeValue, 1f, 1f);
+        healthText.text = currentHP.RuntimeValue + " / " + maxHP.RuntimeValue;
+        InvokeRepeating("TestHealthBar", 0.5f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TestHealthBar()
+    {
+        currentHP.RuntimeValue -= 6.66f;
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        float current = currentHP.RuntimeValue;
+        float max = maxHP.RuntimeValue;
+
+        // not elegant but it works
+        if (current / max < 0)
+        {
+            healthText.text = 0 + " / " + maxHP.RuntimeValue;
+            transform.localScale = new Vector3(0, 1f, 1f);
+            return;
+        }
+
+        transform.localScale = new Vector3(current / max, 1f, 1f);
+        healthText.text = current + " / " + max;
+    }
+
+}
---
+++ games/no-name-yet [2 WEEKS]/Assets/ScriptableObjets/FloatVariable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// from https://unity.com/how-to/architect-game-code-scriptable-objects
+[CreateAssetMenu]
+public class FloatVariable : ScriptableObject, ISerializationCallbackReceiver
+{
+    public float InitialValue;
+
+    [NonSerialized]
+    public float RuntimeValue;
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = InitialValue;
+    }
+
+    public void OnBeforeSerialize() { }
+}
