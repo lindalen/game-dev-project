4464b7f1cb55c31fc2693f0be03c30f1638cea24
Global Hacker for all my repos <email@adress.personal>
2020-08-24 21:46:54 +0200

    DAY 4(7 days later hehe): Adds exp and level system. Level is determined from player exp and base level cost and level multiplier. UI displays values. MISSING: level up functionality, where on playerexp>nextlevelexp, the next level exp is calculated and level is incremented. Will probably consider some talent point system later.

---
+++ games/no-name-yet [2 WEEKS]/Assets/ExpBarManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExpBarManager : MonoBehaviour
+{
+    [SerializeField] IntVariable playerLevel;
+    [SerializeField] FloatVariable baseLevelExp;
+    [SerializeField] FloatVariable levelMultiplier;
+    [SerializeField] FloatVariable playerExp;
+
+    BigNumberFormatter formatter;
+    [SerializeField] Text expText;
+    [SerializeField] Text levelText;
+
+    float nextLevelExp;
+
+    void Awake()
+    {
+        formatter = GetComponent<BigNumberFormatter>();
+        if (playerExp.RuntimeValue<baseLevelExp.RuntimeValue)
+        {
+            nextLevelExp = baseLevelExp.RuntimeValue;
+            playerLevel.RuntimeValue = 1;
+        } else
+        {
+            SetLevelFromExp();
+        }
+    }
+
+    private void Start()
+    {
+        UpdateExpBar();
+        UpdateExpText();
+        UpdateLevelText();
+        Debug.Log("Level is " + playerLevel.RuntimeValue);
+    }
+
+    private void SetLevelFromExp()
+    {
+        nextLevelExp = baseLevelExp.RuntimeValue; //not completely sure about this
+        while (playerExp.RuntimeValue>nextLevelExp)
+        {
+            nextLevelExp *= levelMultiplier.RuntimeValue;
+            playerLevel.RuntimeValue+=1;
+            Debug.Log("Level is now " + playerLevel.RuntimeValue);
+        }
+    }
+
+    private void UpdateExpBar()
+    {
+        transform.localScale = new Vector3(playerExp.RuntimeValue / nextLevelExp, 1f, 1f);
+    }
+
+    private void UpdateExpText()
+    {
+        Debug.Log("Updating text");
+        expText.text = formatter.GetFormattedNumber(playerExp.RuntimeValue) + " / " + formatter.GetFormattedNumber(nextLevelExp);
+    }
+
+    private void UpdateLevelText()
+    {
+        levelText.text = ""+playerLevel.RuntimeValue;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
