02ed1c77bac6de4c66b46851b894e46825c85ca5
d.lindalen <dlindalen@hotmail.com>
2020-08-31 14:23:08 +0200

    DAY 6: Adds placeholder sprites for healer, attacker and defender. Adds unfinished scripts for upgrading healer/attacker/defender.

---
+++ games/no-name-yet [2 WEEKS]/Assets/MakeButtonSizeIntoSpriteSize.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MakeButtonSizeIntoSpriteSize : MonoBehaviour
+{
+    private Button attachedButton;
+    private Sprite attachedSprite;
+
+    void Awake()
+    {
+        //attachedButton = GetComponent<Button>();
+        //attachedSprite = GetComponent<SpriteRenderer>().sprite;
+    }
+
+    void Start()
+    {
+        //ChangeButtonSize();
+    }
+
+    private void ChangeButtonSize()
+    {
+        attachedButton.image.rectTransform.sizeDelta = attachedSprite.rect.size;
+    }
+}
